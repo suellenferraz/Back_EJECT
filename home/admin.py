228183a55b51b22bc247b8773e055b2b8e8a133b
Adriano Filho <adrianoarruda2931@gmail.com>
2024-04-07 22:30:21 -0300

    feat: pagina_home dinamizada

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Bem_vindo, Titulo_cursos_mais_populares, Cursos_mais_populares, Nossa_historia, Alunos, Contato
 
 # Register your models here.
+
+admin.site.register(Bem_vindo)
+admin.site.register(Titulo_cursos_mais_populares)
+admin.site.register(Cursos_mais_populares)
+admin.site.register(Nossa_historia)
+admin.site.register(Alunos)
+admin.site.register(Contato)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from .models import Bem_vindo, Titulo_cursos_mais_populares, Cursos_mais_populares, Nossa_historia, Alunos, Contato
 # Create your views here.
+def home(request):
+
+    bem_vindo = Bem_vindo.objects.last()
+    titulo_cursos = Titulo_cursos_mais_populares.objects.last()
+    cursos_populares = Cursos_mais_populares.objects.all()
+    nossa_historia = Nossa_historia.objects.last()
+    alunos = Alunos.objects.all()
+    contato = Contato.objects.last()
+
+    context = {
+        'bemVindo': bem_vindo,
+        'titulo_curso': titulo_cursos,
+        'cursos_populares':cursos_populares,
+        'nossa_historia':nossa_historia,
+        'alunos':alunos,
+        'contato':contato
+
+
+        
+    }
+
+    return render(request, "index.html", context)
